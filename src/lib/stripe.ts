bb74f60d65bfd2ea60cd612b2b7f2adacc7633ce
webdevhridoy <rafiqulislam11002@gmail.com>
2026-04-26 20:26:36 +0600

    Fixed Vercel Stripe Compiler Crash

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "sk_test_fallback", {
   apiVersion: "2024-06-20",
   appInfo: {
     name: "Sawti TTS SaaS",
